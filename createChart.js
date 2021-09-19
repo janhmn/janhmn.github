712bbefd14c3e38ee2e20c1dafef8cc939e8a079
janhmn <janheumann@Jans-MacBook-Air.local>
2021-09-19 18:07:53 +0200

    Update Page

--- createChart.js
+++ createChart.js
@@ -5,7 +5,7 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
 
 var data = new google.visualization.DataTable();data.addColumn('string', 'Name');data.addColumn('number', 'Wins');
-data.addRows([['Jan', 8],['Lisa', 7],['Jannik',3], ['Philip', 3]]);
+data.addRows([['Jan', 8],['Lisa', 12],['Jannik',4], ['Philip', 5]]);
 
 var options = {'title':'Anzahl Siege pro Spieler','width':500,'height':300};
 
