162faed50262c5f7f3414f2c9b3a065e738846c9
janhmn <jan.hmn@googlemail.com>
2021-12-06 19:49:02 +0100

    Update Page

--- createChart.js
+++ createChart.js
@@ -5,7 +5,7 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
 
 var data = new google.visualization.DataTable();data.addColumn('string', 'Name');data.addColumn('number', 'Wins');
-data.addRows([['Jan', 16],['Lisa', 15],['Jannik',9], ['Philip', 10]]);
+data.addRows([['Jan', 16],['Lisa', 15],['Jannik',10], ['Philip', 11]]);
 
 var options = {'title':'Anzahl Siege pro Spieler','width':500,'height':300};
 
